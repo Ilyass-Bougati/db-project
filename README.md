a88f2e9fce011bd2482ceb3b368dc32b5702d1a3
Ilyass Bougati <i.bougati12@gmail.com>
2026-04-09 08:44:29 +0100

    Small fixes and usefull scripts

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # DB Project
+To run the project use the command
+```bash
+docker compose up -d --build
+```
+This will detach you from the containers, however you can always keep an eye on them through other tools such as lazydocker or docker desktop.
---
+++ purge.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/bash
+
+# warning the user
+echo -e "\e[31mWARNING : \e[0m This script will delete all your containers, volumes and networks. You have 5s to stop before you do something that can't be undone"
+sleep 5
+
+# Stop all running containers
+echo -e "\e[33mStopping all running docker containers\e[0m"
+docker stop $(docker ps -a -q)
+
+# delete all containers
+echo -e "\e[33mDeleting all running docker containers\e[0m"
+docker rm -fv $(docker ps -a -q)
+
+# delete all volumes
+echo -e "\e[33mDeleting all volumes\e[0m"
+docker volume prune --all -f
+
+# delete all networks
+echo -e "\e[33mDeleting all networks\e[0m"
+docker network prune -f
